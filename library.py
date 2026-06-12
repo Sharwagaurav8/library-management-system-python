b14596d1431b5af2cd34f6e5ef6b1cac71fe605c
_sharwagaurav <162777976+Sharwagaurav8@users.noreply.github.com>
2026-06-12 21:17:21 +0530

    Add files via upload

---
+++ library.py
@@ -0,0 +1,50 @@
+books = []
+
+def show_menu():
+    print(">-----<     LIBRARY MANAGEMENT SYSTEM     >-----<")
+    print("1. Add Book")
+    print("2. View Books")
+    print("3. Search Book")
+    print("4. Exit")
+    
+def add_book():
+    book = input("Enter book name : ")
+    books.append(book)
+    print("✅ Book added successfully!")
+    
+def view_books():
+    print("\nBooks in Library : ")
+        
+    for book in books:
+        print(book)
+    
+def search_book():
+    search = input("Enter book name to search : ")
+    
+    if search in books:
+        print("Book found!")
+        
+    else:
+        print("Book not found!")
+
+while True:
+    show_menu()
+    
+    choice = input("Enter your choice : ")
+    
+    if choice == '1':
+        add_book()
+        
+    elif choice == '2':
+        view_books()
+        
+    elif choice == '3':
+        search_book()
+        
+    elif choice == '4':
+        print("Thank you for using the Library Management System.")
+        break
+    
+    else:
+        print("Invalid choice! Please try again.")
+        
